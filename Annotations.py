4ade6732bb4867713b14b3b14cbee558e9f575cb
Buntu William <williambuntu1@gmail.com>
2023-08-30 18:30:40 +0200

    first commit

---
+++ Annotations.py
@@ -0,0 +1,10 @@
+def multiply(m : int) -> int:
+    m = m*56
+    return m
+
+print(multiply(45.7))
+
+
+count : float = 34
+
+print(type(count))
---
+++ Exceptions.py
@@ -0,0 +1,30 @@
+filename = './test.txt'
+filename1 = './test1.txt'
+
+try:
+   file = open(filename, 'r')
+   content = file.read()
+   print(content)
+
+finally:
+    file.close()
+
+
+try:
+    with open(filename1, 'r') as file:
+        content_1 = file.read()
+        print(content_1)
+
+except FileNotFoundError:
+    print('FileNotFoundError')
+# except ZeroDivisionError:
+#     print("ZeroDivisionError")
+
+
+# except NameError:
+#     print("NameError : y must be a number")
+
+# to install a package
+# pip install 'packages'
+# pip install -U packages
+# pip show package
