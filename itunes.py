dfec62a6233382b254063d3318ea6df0efab7cfd
Buntu William <williambuntu1@gmail.com>
2023-09-11 18:26:53 +0200

    another commit

--- itunes.py
+++ itunes.py
@@ -6,4 +6,8 @@ if len(sys.argv) != 2:
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
 
-print(json.dumps(response.json(), indent=2))
+# print(json.dumps(response.json(), indent=2))
+
+o = response.json()
+for result in o['results']:
+    print(result['trackName'])
